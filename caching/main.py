c8db10e50dcbfa89869339baf4f62a532c151a31
Boris Tvaroska <boris@tvaroska.sk>
2024-09-04 14:53:59 +0000

    bug: file upload

--- caching/main.py
+++ caching/main.py
@@ -4,52 +4,30 @@
     User upload PDF - gemini create summarization
 
 """
-
 import os
-import uuid
 import flet as ft
-
-from datetime import timedelta
-
-from google.cloud import storage
 
 from vertexai.generative_models import GenerativeModel, Part
 
-BUCKET = "btvaroska-llm"
-PREFIX = "tmp"
-
 def main(page: ft.Page):
 
-    gcs = storage.Client()
     model = GenerativeModel(model_name="gemini-1.5-pro-001")
 
     def get_file(e: ft.FilePickerResultEvent):
         if e.files:
-            f = e.files[0]
-            # TODO: upload file to GCS
-            bucket = gcs.bucket(BUCKET)
-            dir = str(uuid.uuid4())[:8]
-            fname = f'{PREFIX}/{dir}/{f.name}'
-            blob = bucket.blob(fname)
-            blob_uri = f'gs://{BUCKET}/{fname}'
-
-            signed_url = blob.generate_signed_url(
-                version="v4",
-                # This URL is valid for 15 minutes
-                expiration=timedelta(minutes=15),
-                method="PUT",
-                content_type="application/octet-stream"
-            )
+            file = e.files[0]
 
             upload_file = ft.FilePickerUploadFile(
-                f.name,
-                upload_url=signed_url,
+                file.name,
+                upload_url=e.page.get_upload_url(file.name, 60)
             )
 
             pick_file_dialog.upload([upload_file])
             
             text_part = Part.from_text('Summarize content of attached paper into three paragraphs')
-            pdf_part = Part.from_uri(blob_uri, 'application/pdf')
+            with open(f'/tmp/flet/{file.name}', 'rb') as f:
+                data = f.read()
+            pdf_part = Part.from_data(data, 'application/pdf')
             response = model.generate_content(contents=[text_part, pdf_part])
             selected_files.value = response.candidates[0].content.parts[0].text
         else:
@@ -86,4 +64,4 @@ def main(page: ft.Page):
     )
 
 os.environ['FLET_SECRET_KEY'] = 'SECRET_KEY'
-ft.app(main, view=ft.AppView.WEB_BROWSER)
+ft.app(main, upload_dir='/tmp/flet', view=ft.AppView.WEB_BROWSER)
